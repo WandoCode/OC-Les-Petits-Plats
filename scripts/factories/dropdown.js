32b680fddd10de3ed9a1d2a724107a7c93399fd6
wandole <maxime_chirez@hotmail.com>
2022-08-06 00:21:11 +0200

    Comments added and improve readability

--- scripts/factories/dropdown.js
+++ scripts/factories/dropdown.js
@@ -1,25 +1,32 @@
+/* Return arrays with ingredients present in the recipes */
+/* 'recipes' is an array */
 const dropdownsFactory = (recipes) => {
-  /* recipes is an array */
-
-  /* Return arrays with ingredients present in the recipes */
   const getIngredientsArray = (filter) => {
     let ingredientsSet = new Set();
+
     recipes.forEach((recipe) => {
       const ingredients = recipe.ingredients;
+
       ingredients.forEach((ingredient) => {
         const item = ingredient.ingredient.toLowerCase();
+
+        // Apply string filtering
         const addItem = filter ? item.includes(filter.toLowerCase()) : true;
         if (addItem) ingredientsSet.add(item);
       });
     });
+
     return Array.from(ingredientsSet);
   };
 
   /* Return arrays with appliances present in the recipes */
   const getAppliancesArray = (filter) => {
     let appliancesSet = new Set();
+
     recipes.forEach((recipe) => {
       const item = recipe.appliance.toLowerCase();
+
+      // Apply string filtering
       const addItem = filter ? item.includes(filter.toLowerCase()) : true;
       if (addItem) appliancesSet.add(item);
     });
@@ -30,29 +37,35 @@ const dropdownsFactory = (recipes) => {
   /* Return arrays with ustensils present in the recipes */
   const getUstensilsArray = (filter) => {
     let ustensilsSet = new Set();
+
     recipes.forEach((recipe) => {
       const ustensils = recipe.ustensils;
+
       ustensils.forEach((ustensil) => {
         const item = ustensil.toLowerCase();
+
+        // Apply string filtering
         const addItem = filter ? item.includes(filter.toLowerCase()) : true;
         if (addItem) ustensilsSet.add(item);
       });
     });
+
     return Array.from(ustensilsSet);
   };
 
-  /* Return list element to fill dropdown menus. 'type' give the focused menu  */
+  /* Return list element to fill dropdown menus. 
+  'type': the focused menu
+  'filter': optional, a string to increase filtering  */
   const getDropdownListDOM = (type, filter) => {
-    //TODO: ajouter un autre argument pour filtrer encore plus en fct des caractères entré dans le input (à répercuter dans les fct get****array())
-    let ElementsArray;
+    let elementsArray;
 
-    if (type === "ingredients") ElementsArray = getIngredientsArray(filter);
-    if (type === "ustensils") ElementsArray = getUstensilsArray(filter);
-    if (type === "appliances") ElementsArray = getAppliancesArray(filter);
+    if (type === "ingredients") elementsArray = getIngredientsArray(filter);
+    if (type === "ustensils") elementsArray = getUstensilsArray(filter);
+    if (type === "appliances") elementsArray = getAppliancesArray(filter);
 
     const elementsList = [];
 
-    ElementsArray.forEach((item) => {
+    elementsArray.forEach((item) => {
       const li = document.createElement("li");
       li.textContent = item;
       elementsList.push(li);
