333151016f021d9afa08a0bbd98559298f066f6c
wandole <maxime_chirez@hotmail.com>
2022-08-11 14:52:37 +0200

    Listen to user input: filter recipes with tags

--- main.js
+++ main.js
@@ -1,12 +1,44 @@
 import { initDropdowns } from "./scripts/utils/dropdown/main.js";
 import { recipes } from "./datas/recipes.js";
 import { searchFactory } from "./scripts/factories/mainSearch.js";
+
+const searchInput = document.querySelector("#search");
+
+searchInput.addEventListener("input", (e) => {
+  const value = e.target.value.trim();
+  if (value.length < 3) return;
+
+  //Collect filters: tags + searchinput
+  //=>Gets tags
+  const tagsDOMArray = Array.from(document.querySelectorAll(".tag__text"));
+  const tagsArray = tagsDOMArray.map((tagDOM) => {
+    return tagDOM.innerText;
+  });
+  //=>Gets inputs
+  const unfilteredInputsArray = value.split(" ");
+  const inputsArray = unfilteredInputsArray.filter((item) => {
+    // "" could be present into the initial array. It's removed here
+    return item.length > 0;
+  });
+
+  //=> Puts every items in an array
+  const filtersArray = [...tagsArray, ...inputsArray];
+
+  //Make a search with mainSearch factory
+  //=> Gets an array of recipes
+  const searchModel = searchFactory(recipes);
+  const foundRecipes = searchModel.filterRecipes(filtersArray);
+
+  console.log(foundRecipes);
+  //Update dropdown with new recipes array
+});
 /* TEST */
 
-initDropdowns(recipes);
-const searchModel = searchFactory(recipes);
-const rep = searchModel.filterRecipes(["poisson", "tomate"]);
-console.log(rep);
+const main = () => {
+  initDropdowns(recipes);
+};
+
+main();
 /* FIN TEST */
 
 /* Quand main searchbar input change ou quand un tag est ajouté
