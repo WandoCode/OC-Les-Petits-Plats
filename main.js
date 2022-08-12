f7f69133968ba9d90e7954da23e5a609c7282682
wandole <maxime_chirez@hotmail.com>
2022-08-12 23:46:10 +0200

    Refactor main.js; add comments; clean code

--- main.js
+++ main.js
@@ -1,85 +1,15 @@
-import {
-  initDropdowns,
-  updateDropdown,
-} from "./scripts/utils/dropdown/main.js";
+import { initDropdowns } from "./scripts/utils/dropdown/main.js";
 import { recipes } from "./datas/recipes.js";
-import { searchRecipesFactory } from "./scripts/factories/searchRecipes.js";
-import { recipeFactory } from "./scripts/factories/recipes.js";
+import { initSearch, triggerSearch } from "./scripts/pages/index.js";
 
-/* DOM nodes */
-const searchInput = document.querySelector("#search");
-const recipesSection = document.querySelector(".recipes");
+const main = () => {
+  // Initialize search logic
+  initSearch();
 
-/* Main search input listener */
-const initSearch = () => {
-  searchInput.addEventListener("input", (e) => {
-    const value = e.target.value.trim();
-    triggerSearch(value);
-  });
+  // Initialize dropdown menus
+  initDropdowns(recipes, triggerSearch);
+
+  return;
 };
 
-/* Run a search with current values and tags */
-const triggerSearch = (value) => {
-  //Collects tags
-  const allTagText = document.querySelectorAll(".tag__text");
-  const tagsDOMArray = Array.from(allTagText);
-  const tagsArray = tagsDOMArray.map((tagDOM) => {
-    return tagDOM.innerText;
-  });
-
-  // Collects main search input
-  let inputsArray = [];
-  if (value.length >= 3) {
-    const unfilteredInputsArray = value.split(" ");
-    inputsArray = unfilteredInputsArray.filter((item) => {
-      // "" could be present into the initial array. It's removed here
-      return item.length > 0;
-    });
-  }
-
-  const filtersArray = [...tagsArray, ...inputsArray];
-  if (filtersArray.length === 0) {
-    setEmptyResults();
-    return;
-  }
-
-  //Make the search
-  const searchModel = searchRecipesFactory(recipes);
-  const foundRecipes = searchModel.filterRecipes(filtersArray);
-
-  if (foundRecipes.length === 0) {
-    setEmptyResults();
-    return;
-  }
-
-  //Display recipes
-  recipesSection.innerHTML = "";
-  foundRecipes.forEach((recipe) => {
-    const recipeModel = recipeFactory(recipe);
-    const recipeCardDOM = recipeModel.getRecipeCardDOM();
-    recipesSection.append(recipeCardDOM);
-  });
-
-  // Update dropdown menus
-  updateDropdown(foundRecipes);
-};
-
-/* Display a message on screen to tell that no recipe has been found */
-const showNoRecipesFound = () => {
-  const h2 = document.createElement("h2");
-  h2.textContent =
-    'Pas de recettes correspondant à votre recherche! Essayer de taper "tarte" ou "oeuf"';
-
-  recipesSection.innerHTML = "";
-  recipesSection.append(h2);
-};
-
-const setEmptyResults = () => {
-  showNoRecipesFound();
-  updateDropdown(recipes);
-};
-// Initialize dropdown menus with all the recipes
-initDropdowns(recipes, triggerSearch);
-
-// Initialize search logic
-initSearch();
+main();
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,4 +1,110 @@
 import { recipeFactory } from "../factories/recipes.js";
+import { updateDropdown } from "../utils/dropdown/main.js";
+import { recipes } from "../../datas/recipes.js";
+import { searchRecipesFactory } from "../factories/searchRecipes.js";
 
-export {};
-//TODO: garder?
+/* DOM nodes */
+const searchInput = document.querySelector("#search");
+const recipesSection = document.querySelector(".recipes");
+
+/* Main search input listener */
+const initSearch = () => {
+  searchInput.addEventListener("input", (e) => {
+    const value = e.target.value.trim();
+    triggerSearch(value);
+  });
+
+  return;
+};
+
+/* Run a search with current values and tags */
+const triggerSearch = (value) => {
+  //Collects filters
+  const tagsArray = collectsTagsFilter();
+  const inputsArray = collectMainInputFilter(value);
+
+  const filtersArray = [...tagsArray, ...inputsArray];
+
+  if (filtersArray.length === 0) {
+    setEmptyResults();
+    return;
+  }
+
+  //Process the search
+  const searchModel = searchRecipesFactory(recipes);
+  const foundRecipes = searchModel.filterRecipes(filtersArray);
+
+  if (foundRecipes.length === 0) {
+    setEmptyResults();
+    return;
+  }
+
+  // Display recipes
+  displayRecipes(foundRecipes);
+
+  // Update dropdown menus
+  updateDropdown(foundRecipes);
+
+  return;
+};
+
+/* Return an array with selected tags */
+const collectsTagsFilter = () => {
+  const allTagText = document.querySelectorAll(".tag__text");
+  const tagsDOMArray = Array.from(allTagText);
+
+  const tagsArray = tagsDOMArray.map((tagDOM) => {
+    return tagDOM.innerText;
+  });
+
+  return tagsArray;
+};
+
+/* Return an array with words from main search input field */
+const collectMainInputFilter = (value) => {
+  let inputsArray = [];
+  if (value.length >= 3) {
+    const unfilteredInputsArray = value.split(" ");
+
+    inputsArray = unfilteredInputsArray.filter((item) => {
+      // "" could be present into the initial array. It's removed here
+      return item.length > 0;
+    });
+  }
+
+  return inputsArray;
+};
+
+/* Display the given recipes at screen */
+const displayRecipes = (recipesToDisplay) => {
+  recipesSection.innerHTML = "";
+  recipesToDisplay.forEach((recipe) => {
+    const recipeModel = recipeFactory(recipe);
+
+    const recipeCardDOM = recipeModel.getRecipeCardDOM();
+
+    recipesSection.append(recipeCardDOM);
+  });
+
+  return;
+};
+
+/* Display a message on screen to tell that no recipe has been found */
+const showNoRecipesFound = () => {
+  const h2 = document.createElement("h2");
+  h2.textContent =
+    'Pas de recettes correspondant à votre recherche! Essayer de taper "tarte" ou "oeuf"';
+
+  recipesSection.innerHTML = "";
+  recipesSection.append(h2);
+
+  return;
+};
+
+const setEmptyResults = () => {
+  showNoRecipesFound();
+  updateDropdown(recipes);
+
+  return;
+};
+export { initSearch, triggerSearch };
